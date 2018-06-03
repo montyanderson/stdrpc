c8451c4c5cdf901502230e55477003baa44f6719
Monty Anderson <montydev@protonmail.com>
2018-06-03 18:22:16 +0100

    built out tests

--- test/index.js
+++ test/index.js
@@ -1,8 +1,44 @@
 const assert = require("assert");
+const Koa = require("koa");
+const json = require("koa-json");
+const bodyParser = require("koa-bodyparser");
 const stdrpc = require("../");
 
+const app = new Koa();
+
+app.use(bodyParser());
+app.use(json());
+
+app.use(async ctx => {
+	const { id, method, params } = ctx.request.body;
+
+	try {
+		const result = ({
+			sum: (a, b) => a + b
+		})[method](...params);
+
+		ctx.body = {
+			id,
+			result
+		};
+	} catch({ message }) {
+		ctx.body = {
+			id,
+			error: message
+		};
+	}
+});
+
+app.listen(8000);
+
 describe("stdrpc", () => {
-	it("should return a new object", () => {
-		assert.equal(typeof stdrpc("http://localhost:8080"), "object");
+	const rpc = stdrpc("http://localhost:8000");
+
+	it("should send and receive correct request and response", async () => {
+		assert.equal(await rpc.sum(100, 200), 300);
+	});
+
+	it("should fail on bad method", async () => {
+		await assert.rejects(async () => rpc.divide());
 	});
 });
